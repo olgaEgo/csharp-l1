f52136028cb57c75d29ae9e896555f507599a7f6
OlgaKoroleva <olga.s.egorova@gmail.com>
2022-09-14 15:52:03 +0300

    Ex005

---
+++ Example005_If-Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура! Маша");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
